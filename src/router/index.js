1d4f83f23ca08433079b60dc0a36580cda9aeae6
郑志旭 <516532458@qq.com>
2019-01-02 09:04:31 +0800

    初始化

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import login from '../components/login.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: login
+    },
+    {
+      name:'login',
+      path:"/login",
+      component:login
+    }
+  ]
+})
